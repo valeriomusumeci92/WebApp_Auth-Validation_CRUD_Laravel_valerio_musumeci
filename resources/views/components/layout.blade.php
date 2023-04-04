3bc227d88d0668e34917ca542054111358884df9
Vincenzo Carbonara <vincenzocarbonara07@gmail.com>
2023-04-04 12:33:16 +0200

    Inizializzato il processo con components, layout e navbar

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Presto.it</title>
+    @vite(['resources/css/app.css' , 'resources/js/app.js'])
+</head>
+<body>
+    
+    {{ $slot }}
+
+</body>
+</html>
