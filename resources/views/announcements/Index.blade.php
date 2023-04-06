e5002f3198723334aacb4abd5ccd53e927922069
Vincenzo Carbonara <vincenzocarbonara07@gmail.com>
2023-04-06 08:33:34 +0200

    midificate card sezione annunci

--- resources/views/announcements/Index.blade.php
+++ resources/views/announcements/Index.blade.php
@@ -7,19 +7,20 @@
             </div>
         </div>
     </div>
-    <div class="container">
-        <div class="row">
+    <div class="container text-center">
+        <div class="row align-content-center">
             @foreach ($announcements as $announcement)
-            <div class="col-12 col-md-4 my-4">
+            <div class="col-12 col-md-4 my-4 d-flex justify-content-center">
                 <div class="card shadow" style="width: 18rem;">
                     <img src="https://picsum.photos/200" class="card-img-top p-3 rounded" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">{{$announcement->title}} </h5>
                         <p class="card-text">{{$announcement->body}}  </p>
                         <p class="card-text">{{$announcement->price}}</p>
-                        <a href="{{ route('announcements.show' , compact('announcement'))}}" class="btn btn-primary shadow">Visualizza</a>
-                        <a href="{{route('categoryShow' , ['category'=>$announcement->category])}}" class="my-2 border-top pt-2 border-dark card-link shadow btn btn-success">Categoria: {{ $announcement->category->name }}</a>
-                        <p class="card-footer">Pubblicato il: {{ $announcement->created_at->format('d/m/Y')}} - Autore {{ $announcement->user->name ?? '' }}</p>
+                        
+                        <a href="{{route('categoryShow' , ['category'=>$announcement->category])}}" class="my-2 border-top pt-2 border-dark card-link shadow btn btn-category-custom">Categoria: {{ $announcement->category->name }}</a>
+                        <a href="{{ route('announcements.show' , compact('announcement'))}}" class="btn btn-go-to-custom border-dark shadow">Visualizza</a>
+                        <p class="card-footer">Pubblicato il: {{ $announcement->created_at->format('d/m/Y')}}  Autore {{ $announcement->user->name ?? '' }}</p>
                     </div>
                 </div>
             </div>
