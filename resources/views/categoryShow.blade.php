e04c718b41df0089e063dd42f147c557f16115ad
Vincenzo Carbonara <vincenzocarbonara07@gmail.com>
2023-04-06 08:51:57 +0200

    fixati alcuni bug

--- resources/views/categoryShow.blade.php
+++ resources/views/categoryShow.blade.php
@@ -10,26 +10,26 @@
 <div class="container">
     <div class="row">
         <div class="col-12">
-            <div class="row">
+            <div class="row align-content-center">
                 @forelse ($category->announcements as $announcement)
-                <div class="col-12 col-md-4 my-2">
-                    <div class="card shadow" style="width: 18rem;">
+                <div class="col-12 col-md-4 d-flex justify-content-center my-2">
+                    <div class="card text-center shadow" style="width: 18rem;">
                         <img src="https://picsum.photos/200" class="card-img-top p-3 rounded" alt="...">
                         <div class="card-body">
                             <h5 class="card-title"> {{$announcement->title}}</h5>
                             <p class="card-text">{{$announcement->body}}</p>
-                            <a href="" class="btn btn-primary shadow"> Visualizza</a>
+                            <a href="" class="btn btn-go-to-custom my-2 border-dark shadow"> Visualizza</a>
                             <p class="card-footer my-2">Pubblica il: {{$announcement->created_at->format('d/m/Y)')}} - Autore: {{ $announcement->user->name ?? ''}}
                             </p>
                         </div>
                     </div>
                 </div>
-                    @empty
-                    <div class="col-12">
-                        <p class="h1">Non sono presenti annunci per questa categoria! </p>
-                        <p class="h2">Pubblicane uno: <a href="{{ route('announcements.create')}}" class="btn btn-success shadow">Nuovo annuncio</a></p>
-                    </div>
-                    @endforelse
+                @empty
+                <div class="col-12">
+                    <p class="h1">Non sono presenti annunci per questa categoria! </p>
+                    <p class="h2">Pubblicane uno: <a href="{{ route('announcements.create')}}" class="btn btn-success shadow">Nuovo annuncio</a></p>
+                </div>
+                @endforelse
             </div>
         </div>
     </div>
