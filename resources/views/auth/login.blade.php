d5242aafd98fe6edaf72e39eb859504485fb8820
Vincenzo Carbonara <vincenzocarbonara07@gmail.com>
2023-04-08 08:29:49 +0200

    Form nuovo form login inserito 70%

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,14 +12,14 @@
   
                 <!-- Sign In Form -->
                 @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
                 <form method="POST" action="{{ route ('login') }}">
                   @csrf
                   <div class="form-floating mb-3">
@@ -52,6 +52,28 @@
           </div>
         </div>
       </div>
+    </div>
+  </div>
+
+
+  <div class="container container-reg-custom">
+    <div class="wrapper">
+      <div class="title"><span>Login Form</span></div>
+      <form action="#">
+        <div class="row">
+          <i class="fas fa-user"></i>
+          <input type="text" placeholder="Email" required>
+        </div>
+        <div class="row">
+          <i class="fas fa-lock"></i>
+          <input type="password" placeholder="Password" required>
+        </div>
+        <div class="pass"><a href="#">Forgot password?</a></div>
+        <div class="row button">
+          <input type="submit" value="Login">
+        </div>
+        <div class="signup-link">Not a member? <a href="#">Signup now</a></div>
+      </form>
     </div>
   </div>
   
